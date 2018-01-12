5256e36f521378ae453b22ffa6118268d9a46e6f
ficristo <ficristo.work@gmail.com>
2018-01-12 20:01:36 +0100

    Convert CodeInspectionResultType to be an enum string

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,8 @@
-export type CodeInspectionResultType = 'problem_type_error' | 'problem_type_warning' | 'problem_type_meta';
+export enum CodeInspectionResultType {
+  ERROR = 'problem_type_error',
+  WARNING = 'problem_type_warning',
+  META = 'problem_type_meta'
+}
 
 export interface CodeInspectionPosition {
   line: number;
